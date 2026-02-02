8b65418e5a6b028cde7460f69055a4bc77365f2f
marustas <marudov.stas@gmail.com>
2026-02-02 13:40:47 +0200

    feat(cabins): dynamic route params

--- app/components/CabinCard.tsx
+++ app/components/CabinCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function CabinCard({ cabin }: { cabin: any }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
@@ -40,12 +42,12 @@ function CabinCard({ cabin }: { cabin: any }) {
         </div>
 
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+          <Link
             href={`/cabins/${id}`}
             className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
