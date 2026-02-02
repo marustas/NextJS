f282d3fe9714be3f87047f84a5f280ce93753026
marustas <marudov.stas@gmail.com>
2026-02-02 13:53:44 +0200

    feat: handle not found errors

--- app/_components/SignOutButton.tsx
+++ app/_components/SignOutButton.tsx
@@ -1,3 +1,5 @@
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+
 function SignOutButton() {
   return (
     <button className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full">
