22364be235d64b1d7bc3528b6fe85d1304074e79
marustas <marudov.stas@gmail.com>
2026-02-02 10:10:09 +0200

    feat: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,20 @@ import { PropsWithChildren } from "react";
 import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
 import "@/app/_styles/globals.css";
+import { Metadata } from "next";
 
-export const metadata = {
-  title: "The Wild Oasis",
+export const metadata: Metadata = {
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "The Wild Oasis",
+  },
+  description: "Discover your perfect getaway at The Wild Oasis.",
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-900 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
